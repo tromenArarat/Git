d7c116029a2651fb03a6e2eef209e14d326a07ea
Tomás Manoukian <tomanoukian@gmail.com>
2025-07-04 22:19:27 -0300

    ajustes de estilo

--- TALLER CALCULADORA/APP/script.js
+++ TALLER CALCULADORA/APP/script.js
@@ -22,7 +22,7 @@ const shift = document.getElementById('shift');
 
 shift.addEventListener('click',convertirShift);
 
-const botons = ['MODO','CALC','∫','x⁻¹','log','ᵇ⁄c','√','x²','^','log','ln','(-)','grados','hyp','sin','cos','tan','RCL','ENG',')','(','S⇄D','M+','7','8','9','DEL','AC','4','5','×','÷','1','2','3','+','-','0','.','×10ˣ','Ans']; 
+const botons = ['MODO','CALC','∫','x⁻¹','log','ᵇ⁄c','√','x²','^','log','ln','(-)',"°'''",'hyp','sin','cos','tan','RCL','ENG',')','(','S⇄D','M+','7','8','9','DEL','AC','4','5','×','÷','1','2','3','+','-','0','.','×10ˣ','Ans']; 
 const alternativs = ['config','SOLVE','deriv','fact','sumat','aᵇ⁄c','cubic','cubo','raiz','10^','e^','ang','left','Abs','sin-1','cos-1','tan-1','STO','izq','%',',','aᵇ⁄c-ᵇ⁄c','M-','CONST','CONV','CLR','INS','APAG','MATRIX','VECTOR','nPr','nCr','STAT','CMPLX','BASE','Pol','Rec','Rnd','Rand#','pi','DRGflecha'];
 
 function convertirShift(){
@@ -30,7 +30,7 @@ function convertirShift(){
         let aReemp = document.getElementById(`${botons[i]}`);
         if (aReemp.textContent == botons[i]) {
             aReemp.textContent = alternativs[i];
-            aReemp.style.backgroundColor = 'red';
+            aReemp.style.backgroundColor = 'orange';
         } else {
             aReemp.textContent = botons[i];
             aReemp.style.backgroundColor = 'white';
